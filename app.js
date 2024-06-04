68433eaec18cdca5a6fd74947f864f4d16bda9bf
Samal <56509582+FransoArbela@users.noreply.github.com>
2024-06-05 00:23:40 +0200

    burger wont disappear on scroll

--- app.js
+++ app.js
@@ -2,8 +2,14 @@
 window.onload = function ( ) {
     const burger_button = document.querySelector('.burger-button');
     const mobile_nav = document.querySelector('.mobile-nav');
+
+
     burger_button.addEventListener('click', function(){
         burger_button.classList.toggle('active');
+
+
+
+
         burger_button.style.backGroundColor = 'white';
         mobile_nav.classList.toggle('is-active');
     });
